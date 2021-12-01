96df33e82fd57a9aebee7d6fd813aca6696c2c9b
densil-phd <1206105071@qq.com>
2021-12-01 11:50:05 +0800

    test

--- src/relay/backend/te_compiler.cc
+++ src/relay/backend/te_compiler.cc
@@ -361,6 +361,7 @@ TECompiler& TECompiler::Global() {
 TVM_REGISTER_PASS_CONFIG_OPTION("relay.backend.use_auto_scheduler", Bool);
 TVM_REGISTER_PASS_CONFIG_OPTION("relay.backend.use_meta_schedule", Bool);
 TVM_REGISTER_PASS_CONFIG_OPTION("relay.backend.alt_optimizing", Bool);
+TVM_REGISTER_PASS_CONFIG_OPTION("relay.backend.use_alt", Bool);
 
 TVM_REGISTER_GLOBAL("relay.backend._TECompilerGlobal").set_body_typed([]() {
   return TECompiler::Global();
